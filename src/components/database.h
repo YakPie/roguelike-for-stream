5518d95bd2b55d9e69c761fd0206b30b556b1888
Frank Lyder Bredland <fl@nyggen.com>
2018-09-09 16:22:43 +0200

    Work towards #23. Columns and datatypes.
    Started adding columns and datatypes to the database.

--- src/components/database.h
+++ src/components/database.h
@@ -5,11 +5,22 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <stdarg.h>
+
+// Lookup table of predefined datatypes
+
+struct Datatype
+{
+	char *name;
+	size_t size;
+	// TODO: think about alignment
+	// how to print
+};
 
 struct Column
 {
 	char *name;
-	//  size, offset, datatype
+	struct Datatype type;
 	// void *data;
 };
 
@@ -17,6 +28,7 @@ struct Table
 {
 	char *name;
 
+	// TODO: rename to schema?
 	struct Column columns[255];
 	unsigned int number_of_columns;
 
@@ -39,6 +51,7 @@ struct Database_Handle
 struct Query
 {
 	char *table_name;
+	int query_schema;
 	// list column names
 	// filter method (WHERE)
 };
@@ -47,11 +60,39 @@ struct Query
 void query(struct Database_Handle dbh, struct Query query)
 {
 	if(strcmp(query.table_name, "tables") == 0) {
-		printf("Tables:\n");
+		printf("tables names\n");
+		printf("------------\n");
 		for(int i=0; i < dbh.tables->number_of_tables; i++) {
-			printf("- %s\n", dbh.tables->tables[i].name);
+			printf("%s\n", dbh.tables->tables[i].name);
 		}
 	}
+
+	if(query.query_schema) {
+	// Find a table with the same name as in the query
+	for(int i=0; i < dbh.tables->number_of_tables; i++) {
+		if(strcmp(dbh.tables->tables[i].name, query.table_name) == 0) {
+			// Print out all column names
+			printf("column name   datatype name\n");
+			printf("-----------   -------------\n");
+			for(
+				int ic=0;
+				ic < dbh.tables->tables[i].number_of_columns;
+				ic++) {
+				
+				printf(
+					"%s       %s\n",
+					dbh.tables->tables[i].columns[ic].name,
+					dbh.tables->tables[i].columns[ic].type.name
+				);
+
+			}
+			break;
+		}
+	}
+
+	}
+
+	// Query data
 }
 
 // INSERT
@@ -65,7 +106,8 @@ struct Database_Handle new_database()
 	return dbh;
 }
 
-void create_table(struct Database_Handle dbh, char* name)
+void create_table(
+		struct Database_Handle dbh, char* name, int num, ...)
 {
 	assert(dbh.tables != NULL);
 	assert(dbh.tables->number_of_tables < 255);
@@ -75,6 +117,19 @@ void create_table(struct Database_Handle dbh, char* name)
 	};
 
 	dbh.tables->tables[dbh.tables->number_of_tables] = new_table;
+
+	va_list arg_list;
+	va_start(arg_list, num);
+	for(int i=0; i<num; i++) {
+		dbh.tables
+			->tables[dbh.tables->number_of_tables]
+			.columns[i] = va_arg(arg_list, struct Column);
+	}
+	dbh.tables
+			->tables[dbh.tables->number_of_tables]
+			.number_of_columns = num;
+	va_end(arg_list);
+
 	dbh.tables->number_of_tables++;
 }
 
--- src/db_test.c
+++ src/db_test.c
@@ -4,14 +4,40 @@ int main()
 {
 	struct Database_Handle dbh = new_database();
 
-	create_table(dbh, "test");	
-	create_table(dbh, "test2");	
-	create_table(dbh, "test3");	
+	create_table(dbh, "test", 0);	
+	create_table(dbh, "test2", 0);
+
+	struct Datatype datatype_integer = {
+		.name = "integer",
+		.size = sizeof(int)
+	};
+	struct Column column1 = {
+		.name = "Column1",
+		.type = datatype_integer
+	};
+	struct Column column2 = {
+		.name = "Column2",
+		.type = datatype_integer	
+	};
+
+	create_table(
+		dbh,
+		"table_with_columns",
+		2, column1, column2
+	);
 
 	struct Query test_query = {
 		.table_name = "tables"
 	};
 	query(dbh, test_query);
+
+	printf("\n");
+
+	struct Query test_query2 = {
+		.table_name = "table_with_columns",
+		.query_schema = 1
+	};
+	query(dbh, test_query2);
 
 	return 0;
 }
