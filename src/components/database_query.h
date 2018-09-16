299495f65e182d5401abc4c2d4e0c7cf87f24b12
Frank Lyder Bredland <fl@nyggen.com>
2018-09-16 17:31:35 +0200

    Added tokentypes for the query language

--- src/components/database_query.h
+++ src/components/database_query.h
@@ -33,24 +33,47 @@
 
 #include <stdlib.h>
 
-struct YGQL_Token {
+// SELECT QUERY EXAMPLE:
+//    position(entityId, x, y), health(entityId, >0)?
+// INSERT QUERY EXAMPLE:
+//    position(10, 5, 15).
+// DELETE QUERY EXAMPE:
+// 	delete position(10, 5, 15).
+enum YGQL_TokenType {
+	YGQL_TOKENTYPE_NAME,           // [a-zA-Z][a-zA-Z0-9_]*
+	YGQL_TOKENTYPE_OPEN_PARENT,    // (
+	YGQL_TOKENTYPE_CLOSE_PARENT,   // )
+	YGQL_TOKENTYPE_SEPERATOR,      // ,
+	YGQL_TOKENTYPE_QUESTIONMARK,   // ?
+	YGQL_TOKENTYPE_ENDSTATEMENT,   // .
+//	YGQL_TOKENTYPE_DELETE,         // delete
+	YGQL_TOKENTYPE_UNKOWN,         // 
+};
 
+struct YGQL_Token {
+	enum YGQL_TokenType type;
 };
 
 struct YGQL_AST {
 
 };
 
-struct YGQL_Token scanner(char* query) {
+struct YGQL_Token ygql_scanner(char* query) {
 	struct YGQL_Token token = {
-
+		.type = YGQL_TOKENTYPE_UNKOWN 
 	};
+
+	// scanning for token types
 
 	return token;
 }
 
-struct YGQL_AST* parse(char* query) {
+//
+struct YGQL_AST* ygql_parse(char* query) {
 	struct YGQL_AST* ast = calloc(sizeof(struct YGQL_AST), 1);
+
+
+
 	return ast;
 }
 
--- src/db_query_test.c
+++ src/db_query_test.c
@@ -4,6 +4,8 @@
 
 int main()
 {
+	struct YGQL_AST* ast = ygql_parse("position(10, 5, 15).");
+ 
 	printf("Test\n");
 	return 0;
 }
